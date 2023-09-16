f3bde60646cead3fa374af07a7abfc2863f7d22e
SantiagoBoscan07 <santiagoboscan0702@gmail.com>
2023-09-16 09:54:11 -0400

    Add Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
       <header className="App-header">
         <div className="ArrayBar">
         <button className="button">Generate New Array</button>
         <button className="button">Quicksort</button>
         <button className="button">Bubblesort</button>
-        <button className="button">Dark Mode</button>
+        <button className="button" onClick={toggleDarkMode}>Dark Mode</button>
         </div>
       </header>
     </div>
