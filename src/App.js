8540d0143670cf991ed89f59cdc3ce4342cbc7cb
SantiagoBoscan07 <santiagoboscan0702@gmail.com>
2023-09-16 09:42:12 -0400

    Add Custom Buttons

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Hello there hello 
-        </a>
+        <div className="ArrayBar">
+        <button className="button">Generate New Array</button>
+        <button className="button">Quicksort</button>
+        <button className="button">Bubblesort</button>
+        <button className="button">Dark Mode</button>
+        </div>
       </header>
     </div>
   );
