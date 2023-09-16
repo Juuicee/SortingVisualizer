e657da6280a6efc70e416aa2a3d4c35b2d25a7a5
SantiagoBoscan07 <santiagoboscan0702@gmail.com>
2023-09-16 12:16:45 -0400

    Add Play and Stop

--- src/App.js
+++ src/App.js
@@ -15,11 +15,14 @@ function App() {
     <div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
       <header className="App-header">
         <div className="ArrayBar">
+        <input type="range" min="10" max="400" className="custom-range"/>
         <button className="button">Generate New Array</button>
         <button className="button">Quicksort</button>
         <button className="button">Bubblesort</button>
         <button className="button" onClick={toggleDarkMode}>Dark Mode</button>
-        <input type="range" min="12" max="48"/>
+        <button className="button2">Play</button>
+        <button className="button2">Stop</button>
+        
         </div>
       </header>
     </div>
