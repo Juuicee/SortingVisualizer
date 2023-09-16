2e8e0b47a6cf5b9eee59cfc937578997787bc801
SantiagoBoscan07 <santiagoboscan0702@gmail.com>
2023-09-16 03:58:06 -0400

    Test 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Hello 
+          Learn React Hello 2 
         </a>
       </header>
     </div>
