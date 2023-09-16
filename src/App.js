59b5c245db3d9d63cbcba2036048ee71235f8571
adavjr <adavjr05@gmail.com>
2023-09-16 03:48:41 -0400

    Learn React Right Now Text Test
    Testing of text change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Right Now 
         </a>
       </header>
     </div>
