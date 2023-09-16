cb8c7021bfea713e5a27bcd1cf2445883cc4cba7
DanielSantana1234 <danielsantana01189@gmail.com>
2023-09-16 12:51:47 -0400

    fixing import issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+//import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 import './App.css';
-import React, { useState } from 'react';
-import SortingVisualizer from 'SortingVisualizer/src/SortingVisualizer';
 
 function App() {
-  <SortingVisualizer></SortingVisualizer>
+  return ( 
+    <div className="App">
+        <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
 }
 
 export default App;
